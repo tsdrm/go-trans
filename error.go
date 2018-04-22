866409cd5938defceb248c777461f774cc68ee3a
tangs-drm <tangscna@gmail.com>
2018-04-22 11:34:23 +0800

    添加错误类型

--- error.go
+++ error.go
@@ -1,6 +1,7 @@
 package go_trans
 
 const (
+	// Transcode
 	TransOk              = 0
 	TransSystemError     = 1
 	TransTimeout         = 2
@@ -8,14 +9,20 @@ const (
 	TransNotFound        = 4
 	TransTooManyTimes    = 5
 	TransProcessNotExist = 6
+
+	// Http
+	HTTPRequestBodyError   = 100
+	HTTPRequestParamsError = 101
 )
 
 var ErrorCode = map[int]string{
-	TransOk:              "Ok",
-	TransSystemError:     "System error",
-	TransTimeout:         "Time out",
-	TransCommandError:    "Command error",
-	TransNotFound:        "Not found",
-	TransTooManyTimes:    "Too many times",
-	TransProcessNotExist: "Process not exist",
+	TransOk:                "Ok",
+	TransSystemError:       "System error",
+	TransTimeout:           "Time out",
+	TransCommandError:      "Command error",
+	TransNotFound:          "Not found",
+	TransTooManyTimes:      "Too many times",
+	TransProcessNotExist:   "Process not exist",
+	HTTPRequestBodyError:   "Request body error",
+	HTTPRequestParamsError: "Request params error",
 }
